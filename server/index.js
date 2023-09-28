adafcbbbd02897bc35c5e836f2eaf9cdcf71aeb9
Anmol Shukla <anmol.28422@gmail.com>
2023-09-28 23:37:51 +0530

    single chat API

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import chats from "./data/data.js";
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API is working fine.");
 });
 
-app.listen(5000, console.log("Server is running on port 5000 "));
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
