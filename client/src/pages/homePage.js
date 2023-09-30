19a03c43332f7930c34ff26fa3d74d72a38a7289
Anmol Shukla <anmol.28422@gmail.com>
2023-10-01 00:53:34 +0530

    login and signup setup

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,7 +1,57 @@
 import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+import Login from "../components/Auth/Login.js";
+import Signup from "../components/Auth/Signup.js";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        padding="4"
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          fontSize="4xl"
+          fontFamily="Pixelify Sans"
+          textAlign="center"
+          color="black"
+        >
+          SnappY
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p="4" borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab w="50%">Login</Tab>
+            <Tab w="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
