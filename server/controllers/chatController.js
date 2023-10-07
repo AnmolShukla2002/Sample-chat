96d20d1e95adcd0bdc88aafbeca452f1dcb9a7e1
Anmol Shukla <anmol.28422@gmail.com>
2023-10-08 02:42:24 +0530

    some more controllers and routes

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -41,3 +41,118 @@ export const accessChat = async (req, res) => {
     }
   }
 };
+
+export const fetchChats = async (req, res) => {
+  try {
+    chatModel
+      .find({
+        users: { $elemMatch: { $eq: req.user._id } },
+      })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400).send({ error });
+  }
+};
+//[ '6519dd8efdfb80bc93b2e15c', '651bd203a526685363bc2924' ]
+export const createGroupChat = async (req, res) => {
+  try {
+    const { name, users } = req.body;
+    if (!name || !users || !Array.isArray(users) || users.length < 2) {
+      return res.status(400).json({ message: "Invalid data provided" });
+    }
+
+    const groupChat = await chatModel.create({
+      chatName: name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+    const fullGroupChat = await chatModel
+      .findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    return res.status(200).json(fullGroupChat);
+  } catch (error) {
+    console.error("Error creating group chat:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const renameGroup = async (req, res) => {
+  const { chatId, chatName } = req.body;
+
+  const updatedChat = await chatModel
+    .findByIdAndUpdate(
+      chatId,
+      {
+        chatName: chatName,
+      },
+      {
+        new: true,
+      }
+    )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updatedChat) {
+    res.status(404).send("Chat Not Found");
+  } else {
+    res.json(updatedChat);
+  }
+};
+
+export const addToGroup = async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const added = await chatModel
+    .findByIdAndUpdate(
+      chatId,
+      {
+        $push: { users: userId },
+      },
+      {
+        new: true,
+      }
+    )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!added) {
+    res.status(404).send("Chat Not Found");
+  } else {
+    res.json(added);
+  }
+};
+
+export const removeFromGroup = async (req, res) => {
+  const { chatId, userId } = req.body;
+  const removed = await chatModel
+    .findByIdAndUpdate(
+      chatId,
+      {
+        $pull: { users: userId },
+      },
+      {
+        new: true,
+      }
+    )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!removed) {
+    res.status(404).send("Chat Not Found");
+  } else {
+    res.json(removed);
+  }
+};
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,14 +1,21 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
-import { accessChat } from "../controllers/chatController.js";
+import {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
+} from "../controllers/chatController.js";
 
 const router = express.Router();
 
 router.post("/", protect, accessChat);
-// router.get("/", protect, fetchChats);
-// router.post("/group", protect, createGroupChat);
-// router.put("/rename", protect, renameGroup);
-// router.put("/groupremove", protect, removeFromGroup);
-// router.put("/groupadd", protect, addToGroup);
+router.get("/", protect, fetchChats);
+router.post("/group", protect, createGroupChat);
+router.put("/rename", protect, renameGroup);
+router.put("/groupremove", protect, removeFromGroup);
+router.put("/groupadd", protect, addToGroup);
 
 export default router;
